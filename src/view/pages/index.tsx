506b6716e21af5c7630d7008c57431953cb27d62
otakuchan <msqkn310@gmail.com>
2020-01-24 02:39:05 +0900

    Update index.tsx

--- src/view/pages/index.tsx
+++ src/view/pages/index.tsx
@@ -38,8 +38,8 @@ export default class Index extends React.Component<{},{}> {
             <FormattedMessage id="Top.Title"/>
           </Typography>
           <Typography align="center" color="textSecondary" paragraph variant="caption">
-            beta ver0.0.2.9<br/>
-            last update: 2020/01/23 JST
+            beta ver0.0.3.0<br/>
+            last update: 2020/01/24 JST
           </Typography>
           <AddToHomeScreenTicker/>
           <div>
