cb3c3c38fc36220b70a5ac50ace62a7746d0d87f
otakuchan <msqkn310@gmail.com>
2020-01-25 02:18:38 +0900

    Update index.tsx

--- src/view/pages/index.tsx
+++ src/view/pages/index.tsx
@@ -38,8 +38,8 @@ export default class Index extends React.Component<{},{}> {
             <FormattedMessage id="Top.Title"/>
           </Typography>
           <Typography align="center" color="textSecondary" paragraph variant="caption">
-            beta ver0.0.3.0<br/>
-            last update: 2020/01/24 JST
+            beta ver0.0.3.1<br/>
+            last update: 2020/01/25 JST
           </Typography>
           <Typography align="center" color="textSecondary" paragraph variant="caption">
             {_lang() === "en" &&
