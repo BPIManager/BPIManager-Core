2296a77ebdab741cd33de94ea9f8b594e1e7950c
potaku <msqkn310@gmail.com>
2020-01-29 23:46:51 +0900

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,10 @@ ReactDOM.render(<App />, document.getElementById("root"));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+navigator.serviceWorker.getRegistrations().then(function(registrations) {
+ for(let registration of registrations) {
+  registration.unregister()
+} })
 /*
 serviceWorker.register({
   onUpdate: (registration:ServiceWorkerRegistration) => {
