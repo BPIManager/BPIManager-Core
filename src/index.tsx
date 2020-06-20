dea7f181a25709c6f41b442cfe7ba9be15bb1e1a
otakuchan <msqkn310@gmail.com>
2020-06-20 23:25:00 +0900

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,11 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-//import * as serviceWorker from "./serviceWorker";
+import * as serviceWorker from "./serviceWorker";
 //import ReloadModal from './view/components/update';
 
 ReactDOM.render(<App />, document.getElementById("root"));
-
+serviceWorker.unregister();
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
