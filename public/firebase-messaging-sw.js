98c021082e1aabaa0b5d0b0402dc70954c5294a9
otakuchan <msqkn310@gmail.com>
2020-06-20 23:19:16 +0900

    競合解消

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,4 +1,4 @@
-/*
+
 importScripts('https://www.gstatic.com/firebasejs/7.14.4/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/7.14.4/firebase-messaging.js');
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+//import * as serviceWorker from "./serviceWorker";
 import ReloadModal from './view/components/update';
 
 ReactDOM.render(<App />, document.getElementById("root"));
@@ -11,7 +11,7 @@ ReactDOM.render(<App />, document.getElementById("root"));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-
+/*
 serviceWorker.register({
   onUpdate: (registration:ServiceWorkerRegistration) => {
     if (registration.waiting) {
