0db01063212570f7e793ed8454ad85e0e4af8faa
otakuchan <msqkn310@gmail.com>
2020-06-20 22:01:10 +0900

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -22,10 +22,4 @@ if ('serviceWorker' in navigator) {
       }
     },
   });
-  navigator.serviceWorker.register('../firebase-messaging-sw.js')
-  .then(function(registration) {
-      firebase.messaging().useServiceWorker(registration);
-  }).catch(function(err) {
-    console.log('Service worker registration failed, error:', err);
-  });
 }
