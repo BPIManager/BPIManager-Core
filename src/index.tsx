cf7c64a4bc28b44dad1ce11c1f283802a101fdc8
otakuchan <msqkn310@gmail.com>
2020-06-20 23:30:01 +0900

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,10 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+//import * as serviceWorker from "./serviceWorker";
 //import ReloadModal from './view/components/update';
 
 ReactDOM.render(<App />, document.getElementById("root"));
-serviceWorker.unregister();
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
@@ -20,3 +19,10 @@ serviceWorker.register({
   },
 });
 */
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("./firebase-messaging-sw.js").then(function(registration) {
+      console.log("Registration successful, scope is:", registration.scope);
+  }).catch(function(err) {
+    console.log("Service worker registration failed, error:", err);
+  });
+}
