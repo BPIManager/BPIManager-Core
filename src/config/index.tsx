81ebe105c85b4192098441481505df665dad9da9
otakuchan <msqkn310@gmail.com>
2020-08-25 00:19:49 +0900

    Update index.tsx

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -2,7 +2,7 @@ export const config ={
   versionNumber:"55",
   client:"ca-pub-1494631468121976",
   slot:"6952909012",
-  versionString:"beta ver0.0.6.0",
+  versionString:"beta ver0.0.6.0a",
   lastUpdate:"2020/08/23 JST",
   baseUrl:"https://bpi.poyashi.me"
 }
