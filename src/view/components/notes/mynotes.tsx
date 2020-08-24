43d41426241b1af722801b8529282a9a7ec346ad
otakuchan <msqkn310@gmail.com>
2020-08-25 00:23:57 +0900

    Update mynotes.tsx

--- src/view/components/notes/mynotes.tsx
+++ src/view/components/notes/mynotes.tsx
@@ -16,7 +16,6 @@ import Dialog from '@material-ui/core/Dialog';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import DialogContent from '@material-ui/core/DialogContent';
 import TextField from '@material-ui/core/TextField';
-import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogActions from '@material-ui/core/DialogActions';
 import Button from '@material-ui/core/Button';
 import { _currentStore } from '@/components/settings';
