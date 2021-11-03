35d83364b951e003c4db3f95a30568450391f73e
djilyaro <47537864+potakusan@users.noreply.github.com>
2021-11-03 16:34:22 +0900

    richview updated

--- src/view/components/songs/played/beta/richView.tsx
+++ src/view/components/songs/played/beta/richView.tsx
@@ -11,8 +11,10 @@ import { scoresDB } from "@/components/indexedDB";
 import Grid from "@mui/material/Grid";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
-import { Box, Divider, LinearProgress, LinearProgressProps, Pagination, Paper } from "@mui/material";
+import { Box, Divider, FormControl, InputLabel, LinearProgress, LinearProgressProps, MenuItem, Pagination, Paper, Select, SelectChangeEvent } from "@mui/material";
 import timeFormatter, { updatedTime } from "@/components/common/timeFormatter";
+import { loader } from "@/components/rivals/letters";
+import Loader from "@/view/components/common/loader";
 
 interface P{
   data:scoreData[],
@@ -29,7 +31,9 @@ interface S{
   FV:number,
   currentSongData:songData | null,
   currentScoreData:scoreData | null,
-  components:string[]
+  components:string[],
+  isLoading:boolean,
+  winlose:any
 }
 
 export default class SongsRichTable extends React.Component<Readonly<P>,S>{
@@ -43,7 +47,17 @@ export default class SongsRichTable extends React.Component<Readonly<P>,S>{
       currentSongData:null,
       currentScoreData:null,
       components:_currentViewComponents().split(","),
+      isLoading:true,
+      winlose:null
     }
+  }
+
+  async componentDidMount(){
+    const l = await loader();
+    this.setState({
+      winlose:l,
+      isLoading:false
+    })
   }
 
   handleOpen = (updateFlag:boolean,row:songData|scoreData):void=> {
@@ -56,39 +70,54 @@ export default class SongsRichTable extends React.Component<Readonly<P>,S>{
     });
   }
 
-  handleChangeRowsPerPage = (event:React.ChangeEvent<HTMLInputElement>):void => {
+  handleChangeRowsPerPage = (event:SelectChangeEvent<number>,_m:any):void => {
     this.props.handleChangePage(null,0);
-    this.setState({rowsPerPage:+event.target.value});
+    this.setState({rowsPerPage:Number(event.target.value)});
+  }
+
+  change = (_e:any,page:number)=>{
+    if(this.props.page + 1 === page || !page) return;
+    this.props.handleChangePage(_e,page-1)
   }
 
   render(){
-    const {rowsPerPage,isOpen,currentSongData,currentScoreData,FV} = this.state;
+    const {rowsPerPage,isOpen,currentSongData,currentScoreData,FV,winlose,isLoading} = this.state;
     const {page,data} = this.props;
     const c = _currentTheme();
     const bgColor = c === "dark" ? "#0a0a0a" : c === "deepsea" ? "#000d19" : "#fff"
+    if(isLoading){
+      return <Loader/>
+    }
     return (
       <React.Fragment>
-        <Pagination count={Math.ceil(data.length / 10)} page={page+1} color="secondary" onChange={(_e,page)=>{
-          if(this.props.page + 1 === page) return;
-          this.props.handleChangePage(_e,page-1)
-        }}/>
+        <Pagination count={Math.ceil(data.length / rowsPerPage)} page={page+1} color="secondary" onChange={this.change}/>
         <div id="screenCaptureTarget" style={{backgroundColor:bgColor}}>
           <Grid container spacing={2} style={{marginTop:"15px"}}>
             {data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row:scoreData,i:number) => {
               const prefix = _prefix(row.difficulty);
               const f = this.props.allSongsData.get(row.title + prefix);
+              const m = winlose ? winlose.find((item:any)=>item.title === row.title && item.difficulty === row.difficulty) : null;
               if(!f) return (null);
-              return (<Item row={row} song={f} key={i} handleOpen={this.handleOpen}/>)
+              return (<Item row={row} winlose={m || null} song={f} key={row.title + row.difficulty} handleOpen={this.handleOpen}/>)
             })}
           </Grid>
           {isOpen &&
             <DetailedSongInformation isOpen={isOpen} song={currentSongData} score={currentScoreData} handleOpen={this.handleOpen} firstView={FV}/>
           }
         </div>
-        <Pagination count={Math.ceil(data.length / 10)} page={page+1} color="secondary" onChange={(_e,page)=>{
-          if(this.props.page + 1 === page) return;
-          this.props.handleChangePage(_e,page-1)
-        }}/>
+        <Pagination count={Math.ceil(data.length / rowsPerPage)} page={page+1} color="secondary" onChange={this.change}/>
+        <div style={{display:"flex",justifyContent:"flex-end"}}>
+          <FormControl variant="standard">
+            <InputLabel>表示</InputLabel>
+            <Select value={rowsPerPage} onChange={this.handleChangeRowsPerPage}>
+            {
+              [10,20,30].map((item:number)=>{
+                return (<MenuItem key={item} value={item}>{item}</MenuItem>);
+              })
+            }
+            </Select>
+          </FormControl>
+        </div>
       </React.Fragment>
     );
   }
@@ -96,30 +125,46 @@ export default class SongsRichTable extends React.Component<Readonly<P>,S>{
 
 const fontSize = "10px";
 
-class Item extends React.Component<{
+interface IP {
   row:scoreData,
   song:songData,
+  winlose:any,
   handleOpen:(flag:boolean,row:scoreData)=>void
-},{}>{
+}
+
+class Item extends React.Component<IP,{}>{
+
+  bpi = new bpiCalcuator();
+
+  nextBPI = (nextBPI:number)=>{
+    if(this.props.row.currentBPI === Infinity) return ("-");
+    if(nextBPI < 0) nextBPI = 0;
+    const {song} = this.props;
+    const currentScore = this.props.row.exScore || 0;
+    this.bpi.setData(song.notes * 2, song.avg, song.wr);
+    this.bpi.setCoef(song.coef || -1);
+    return currentScore - this.bpi.calcFromBPI(nextBPI,true);
+  }
 
   render(){
-    const {row,song} = this.props;
+    const {row,song,winlose} = this.props;
     const max  = song["notes"] * 2;
     const barColor = diffColor(0,row.clearState);
     const per = (row.exScore / max * 100);
+    let nextBPI = Math.ceil((this.props.row ? this.props.row.currentBPI : -15) / 10) * 10;
     return (
-    <Grid item xs={12} style={{padding:0}}>
+    <Grid item xs={12} sm={12} md={12} lg={6} className="gridWithPad">
       <CardContent style={{padding:0,cursor:"pointer"}} onClick={()=>this.props.handleOpen(false,row)}>
         <Typography component="div" className="spaceBetween" sx={{ fontSize: 14, alignItems:"center"}} color="text.secondary" gutterBottom>
-          <p style={{padding:"4px 0 4px 5px",margin:0,borderLeft:"3px solid " + barColor}}>
+          <p className="withClearLamp" style={{padding:"4px 0 4px 5px",margin:0,borderLeft:"3px solid " + barColor,wordBreak:"break-all"}}>
           <span style={{fontSize:"12px"}}>☆{song.difficultyLevel}</span><br/>
-          {row.title}{_prefix(row.difficulty)}
+          <span className="listHighlighted">{row.title}{_prefix(row.difficulty)}</span>
           </p>
           <div style={{textAlign:"right",padding:"0 0 0 3px",margin:0}}>
           {row.currentBPI !== Infinity && (
             <React.Fragment>
-            <span style={{fontSize:"12px"}}>{new bpiCalcuator().rank(row.currentBPI)}位</span><br/>
-              BPI&nbsp;{row.currentBPI.toFixed(2)}
+              <span style={{fontSize:"12px"}}>{this.bpi.rank(row.currentBPI)}位</span><br/>
+              <span className="listHighlighted">BPI&nbsp;{row.currentBPI.toFixed(2)}</span>
             </React.Fragment>
           )}
           {row.currentBPI === Infinity && (
@@ -131,7 +176,7 @@ class Item extends React.Component<{
           </div>
         </Typography>
         <LinearProgressWithLabel per={per}/>
-        <Paper style={{fontSize:fontSize,padding:"12px"}} square elevation={0}>
+        <Paper style={{fontSize:fontSize,padding:"12px",marginTop:"4px"}} square variant="outlined" elevation={0}>
           <Grid container justifyContent="space-between" style={{margin:"5px 0"}}>
             <Grid item xs={5} sm={5}>
               <div className="spaceBetween"><Typography color="text.secondary">EX SCORE</Typography><span>{row.exScore}</span></div>
@@ -141,15 +186,17 @@ class Item extends React.Component<{
                 </span>
               </div>
               <div className="spaceBetween"><Typography color="text.secondary">MISS COUNT</Typography><span>{row.missCount || 0}</span></div>
+              <div className="spaceBetween"><Typography color="text.secondary">WIN/LOSE</Typography><span>{winlose ? winlose.win + " / " + winlose.lose : "0 / 0"}</span></div>
             </Grid>
             <Grid item xs={5} sm={5}>
               <ScoreCompares row={row}/>
+              <div className="spaceBetween"><Typography color="text.secondary">BPI{nextBPI}</Typography><span>{this.nextBPI(nextBPI)}点</span></div>
             </Grid>
           </Grid>
           <Typography style={{textAlign:"right",fontSize:fontSize}} color="text.secondary">最終更新日&nbsp;{timeFormatter(0,row.updatedAt)}&nbsp;({updatedTime(row.updatedAt)})</Typography>
         </Paper>
-        <Divider style={{margin:"10px 0"}}/>
       </CardContent>
+      <Divider style={{margin:"10px 0"}}/>
     </Grid>
     )
   }
@@ -217,15 +264,15 @@ class ScoreCompares extends React.Component<{row:scoreData},{
       return (
         <React.Fragment>
           <div className="spaceBetween"><Typography color="text.secondary">前回</Typography>
-          <span>
+          <span>…
           </span>
           </div>
           <div className="spaceBetween"><Typography color="text.secondary">前作</Typography>
-          <span>
+          <span>…
           </span>
           </div>
           <div className="spaceBetween"><Typography color="text.secondary">自己歴代</Typography>
-            <span>
+            <span>…
             </span>
           </div>
         </React.Fragment>
