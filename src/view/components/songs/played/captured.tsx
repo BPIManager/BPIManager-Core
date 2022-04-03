73139bf3ec4fa57da9756e16ca1ab358ab3dc1ba
potakusan <47537864+potakusan@users.noreply.github.com>
2022-04-03 18:51:17 +0900

    Use Native API for sharing

--- src/view/components/songs/played/captured.tsx
+++ src/view/components/songs/played/captured.tsx
@@ -3,10 +3,6 @@ import html2canvas from "html2canvas";
 import Backdrop from '@mui/material/Backdrop';
 import Loader from '../../common/loader';
 import { saveAsImage } from '@/components/common/saveImage';
-import Dialog from '@mui/material/Dialog';
-import DialogTitle from '@mui/material/DialogTitle';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
 import List from '@mui/material/List';
 import SaveAltIcon from '@mui/icons-material/SaveAlt';
 import TwitterIcon from '@mui/icons-material/Twitter';
@@ -16,6 +12,8 @@ import bpiCalcuator, { showBpiDist } from '@/components/bpi';
 import statMain from '@/components/stats/main';
 import dayjs from 'dayjs';
 import { config } from '@/config';
+import ListSubheader from '@mui/material/ListSubheader';
+import SwipeableDrawer from '@mui/material/SwipeableDrawer';
 
 interface P {
   close: () => void
@@ -25,7 +23,7 @@ interface S {
   capturing: boolean,
   completed: boolean,
   uploading: boolean,
-  result: string,
+  result: HTMLCanvasElement | null,
   token: string,
   userName: string,
 }
@@ -38,7 +36,7 @@ class Captured extends React.Component<P, S> {
       capturing: true,
       completed: false,
       uploading: false,
-      result: "",
+      result: null,
       token: "",
       userName: ""
     }
@@ -61,15 +59,17 @@ class Captured extends React.Component<P, S> {
 
     document.documentElement.classList.add("hide-scrollbar");
     html2canvas(target, { scale: 2, width: target.clientWidth, height: target.clientHeight }).then(canvas => {
-      const targetImgUri = canvas.toDataURL("image/jpeg");
+      const targetImgUri = canvas;
       document.documentElement.classList.remove("hide-scrollbar");
       this.setState({ capturing: false, completed: true, result: targetImgUri, token: token, userName: (user && user.displayName) ? user.displayName : "" });
     });
   }
 
   save = () => {
-    saveAsImage(this.state.result);
-    this.props.close();
+    if (this.state.result) {
+      saveAsImage(this.state.result.toDataURL("image/jpeg"));
+      this.props.close();
+    }
   }
 
   fetcher = async (endpoint: string, data: string) => {
@@ -88,15 +88,10 @@ class Captured extends React.Component<P, S> {
     });
   }
 
+  isMobile = () => navigator.userAgent.match(/iPhone|Android.+Mobile/)
+
   upload = async () => {
-    this.setState({ uploading: true });
-    const t = await this.fetcher("tweet/upload", this.state.result.replace("data:image/jpeg;base64,", ""));
-
-    const body = await t.json();
-    if (!body || !body.extended_entities || !body.extended_entities.media[0] || !body.extended_entities.media[0].display_url) {
-      alert("画像のアップロードに失敗しました。\n少し経ってからもう一度お試しください。\n(楽曲の表示件数により、画像サイズが大きすぎてアップロードできない場合があります)");
-
-      this.setState({ uploading: false });
+    if (!this.state.result) {
       return;
     }
     const bpi = new bpiCalcuator();
@@ -107,8 +102,36 @@ class Captured extends React.Component<P, S> {
     const rank = bpi.rank(totalBPI, false);
     const rankPer = Math.round(rank / bpi.getTotalKaidens() * 1000000) / 10000;
     const profileURL = this.state.userName ? config.baseUrl + "/r/u/" + this.state.userName : "";
-    const url = body.extended_entities.media[0].display_url;
     const updates = await statsAPI.updatedAtToday();
+
+    if (this.isMobile()) {
+      //Android or iPhone の場合はネイティブAPIを利用
+      this.state.result.toBlob((blob) => {
+        if (!blob) return alert("An error occured while converting image into blob!");
+        const image = new File([blob], 'tmp.png', { type: 'image/png' });
+        navigator.share({
+          text: `本日のスコア更新数：${updates.length || 0}件\n` +
+            `総合BPI:${totalBPI}(前日比:${showBpiDist(totalBPI, lastDay)},前週比:${showBpiDist(totalBPI, lastWeek)})\n推定順位:${rank}位,皆伝上位${rankPer}％\n#BPIM`,
+          url: profileURL,
+          files: [image]
+        }).catch((error) => {
+          console.log(error)
+        })
+      })
+      return this.modalClose();
+    }
+
+    this.setState({ uploading: true });
+    const t = await this.fetcher("tweet/upload", this.state.result.toDataURL("image/jpeg").replace("data:image/jpeg;base64,", ""));
+
+    const body = await t.json();
+    if (!body || !body.extended_entities || !body.extended_entities.media[0] || !body.extended_entities.media[0].display_url) {
+      alert("画像のアップロードに失敗しました。\n少し経ってからもう一度お試しください。\n(楽曲の表示件数により、画像サイズが大きすぎてアップロードできない場合があります)");
+
+      this.setState({ uploading: false });
+      return;
+    }
+    const url = body.extended_entities.media[0].display_url;
     window.open(`https://twitter.com/intent/tweet?text=${(
       `本日のスコア更新数：${updates.length || 0}件%0a` +
       `総合BPI:${totalBPI}(前日比:${showBpiDist(totalBPI, lastDay)},前週比:${showBpiDist(totalBPI, lastWeek)})%0a推定順位:${rank}位,皆伝上位${rankPer}％%0a`
@@ -118,13 +141,21 @@ class Captured extends React.Component<P, S> {
     this.props.close();
   }
 
+  modalClose = () => {
+    //アニメーション表示のため
+    this.setState({ completed: false });
+    setTimeout(() => {
+      this.props.close();
+    }, 600)
+  }
+
   render() {
     const { capturing, completed, uploading } = this.state;
 
     const buttons = [
       { icon: <SaveAltIcon />, primary: "カメラロールに保存", secondary: "画像を端末に保存します", onClick: () => this.save() },
-      { icon: <TwitterIcon />, primary: "Twitterでシェア", secondary: "本日のスコア更新数などのデータとともに画像を共有します", onClick: () => this.upload() },
-      { icon: <CancelIcon />, primary: "閉じる", secondary: "何もしないで終了します", onClick: () => this.props.close() },
+      { icon: <TwitterIcon />, primary: this.isMobile() ? "ソーシャルメディアに共有" : "Twitter でシェア", secondary: "本日のスコア更新数などのデータとともに画像を共有します", onClick: () => this.upload() },
+      { icon: <CancelIcon />, primary: "閉じる", secondary: "何もしないで終了します", onClick: () => this.modalClose() },
     ];
 
     return (
@@ -135,25 +166,27 @@ class Captured extends React.Component<P, S> {
         <Backdrop open={uploading}>
           <Loader text="画像をアップロードしています" />
         </Backdrop>
-        {completed && (
-          <Dialog open={true}>
-            <DialogTitle>{"キャプチャを共有"}</DialogTitle>
-            <DialogContent>
-              <DialogContentText>
-                現在表示中のスコア一覧を画像として保存するか、Twitterでシェアできます。
-              </DialogContentText>
-              <List>
-                {buttons.map((item, i) => {
-                  return (
-                    <DefListCard key={i} onAction={item.onClick} disabled={false} icon={item.icon}
-                      primaryText={item.primary} secondaryText={item.secondary} />
-                  )
-                })
-                }
-              </List>
-            </DialogContent>
-          </Dialog>
-        )}
+        <SwipeableDrawer
+          anchor="bottom"
+          open={completed}
+          onClose={() => this.modalClose()}
+          onOpen={() => null}
+        >
+          <List
+            subheader={
+              <ListSubheader component="div">
+                ライバル管理
+                </ListSubheader>
+            }>
+            {buttons.map((item, i) => {
+              return (
+                <DefListCard key={i} onAction={item.onClick} disabled={false} icon={item.icon}
+                  primaryText={item.primary} secondaryText={item.secondary} />
+              )
+            })
+            }
+          </List>
+        </SwipeableDrawer>
       </React.Fragment>
     );
   }
