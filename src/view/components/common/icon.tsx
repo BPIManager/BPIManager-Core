915eb2a4a715392d234dba26fa1798fdee6e3b16
potakusan <msqkn310@gmail.com>
2022-06-29 22:48:07 +0900

    Update icon.tsx

--- src/view/components/common/icon.tsx
+++ src/view/components/common/icon.tsx
@@ -10,47 +10,19 @@ export const UserIcon: React.FC<{
   disableZoom?: boolean;
   _legacy?: boolean;
   className?: string;
-}> = ({
-  defaultURL,
-  _legacy,
-  altURL,
-  text,
-  size,
-  style,
-  disableZoom,
-  className,
-}) => {
+}> = ({ defaultURL, _legacy, altURL, text, size, style, disableZoom, className }) => {
   const [open, setOpen] = useState<boolean>(false);
   const toggle = () => {
     if (disableZoom) return;
     setOpen(!open);
   };
-  const imgBody = (
-    <img
-      src={defaultURL || "noimage"}
-      style={{ width: "100%", height: "100%" }}
-      alt={text}
-      onError={(e) => ((e.target as HTMLImageElement).src = altURL)}
-    />
-  );
+  const imgBody = <img src={defaultURL || "noimage"} style={{ width: "100%", height: "100%", objectFit: "cover" }} alt={text} onError={(e) => ((e.target as HTMLImageElement).src = altURL)} />;
   return (
     <>
-      <Avatar
-        sx={
-          _legacy
-            ? {}
-            : { ...style, width: size || "100%", height: size || "100%" }
-        }
-        className={className || ""}
-        onClick={toggle}
-      >
+      <Avatar sx={_legacy ? {} : { ...style, width: size || "100%", height: size || "100%" }} className={className || ""} onClick={toggle}>
         {imgBody}
       </Avatar>
-      <Modal
-        open={open}
-        onClose={toggle}
-        sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}
-      >
+      <Modal open={open} onClose={toggle} sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
         <Avatar sx={{ width: "45%", height: "auto" }} onClick={toggle}>
           {imgBody}
         </Avatar>
