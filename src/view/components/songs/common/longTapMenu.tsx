06d5835684ab6f96f154ea6a26851f83780df1d9
potakusan <msqkn310@gmail.com>
2022-08-20 21:27:46 +0900

    Update longTapMenu.tsx

--- src/view/components/songs/common/longTapMenu.tsx
+++ src/view/components/songs/common/longTapMenu.tsx
@@ -6,7 +6,7 @@ import JumpWeb from "../detailScreen/jumpWeb";
 const Menu: React.FC<{ close: () => void; open: boolean; song: songData | null }> = ({ close, open, song }) => {
   if (!song) return null;
   return (
-    <SwipeableDrawer anchor={"bottom"} onOpen={() => null} open={open} onClose={close}>
+    <SwipeableDrawer className="songDrawer" style={{ zIndex: 1560 }} anchor={"bottom"} onOpen={() => null} open={open} onClose={close}>
       <JumpWeb onlyList song={song} />
       <EditList onlyList song={song} closeFunc={close} />
     </SwipeableDrawer>
